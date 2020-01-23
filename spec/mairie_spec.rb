71a4864222af6c7e0f41254ad66d53f7e9c7e5b0
Nazaninf24 <nazaninf24@gmail.com>
2020-01-23 18:36:14 +0100

    last commit

--- spec/mairie_spec.rb
+++ spec/mairie_spec.rb
@@ -1,7 +1,22 @@
 require_relative '../lib/mairie'
 
-describe "the get_email method" do 
-	it "should return the email of a given url" do
-		expect(get_email("http://annuaire-des-mairies.com/95/franconville.html")).to eq("Communication@ville-franconville.fr")
-	end
+describe "the get_townhall_email method" do
+    it "should return a string" do
+        expect(get_townhall_email("https://www.annuaire-des-mairies.com/95/avernes.html").class).to eq(String)
+    end
+    it "should return an email" do
+        expect(get_townhall_email("https://www.annuaire-des-mairies.com/95/avernes.html").include?('@')).to eq(true)
+    end
+end
+
+describe "the get_townhall_urls method" do
+    it "should return an array" do
+        expect(get_townhall_urls.class).to eq(Array)
+    end
+    it "should return URLs" do
+        expect(get_townhall_urls[0].include?('html')).to eq(true)
+    end
+    it "should return more than 3 URLs" do
+        expect(get_townhall_urls.length > 3).to eq(true)
+    end
 end
